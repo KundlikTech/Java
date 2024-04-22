0ec26c5c283c54ec8ffb00d68d7c083727fd4b5c
Kundlik  Gavhane <123533550+KundlikTech@users.noreply.github.com>
2024-04-22 14:18:41 +0530

    Add files via upload

---
+++ Patterns/First.java
@@ -0,0 +1,15 @@
+//   1  2  3  4
+// 1 *  *  *  *
+// 2 *  *  *  *
+// 3 *  *  *  *
+// 4 *  *  *  *
+class First{
+    public static void main(String[] args) {
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                System.out.print("*  ");
+            }
+            System.out.println();
+        }
+    }
+}
